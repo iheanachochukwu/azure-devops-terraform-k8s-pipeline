6f0e1ea014c3fa1c6918071674946acb30578d53
iheanachochukwu <49342621+iheanachochukwu@users.noreply.github.com>
2021-09-19 17:31:47 -0500

    Update main.tf
    removed backend access_key and saved as environment variable

--- configuration/iaac/azure/main.tf
+++ configuration/iaac/azure/main.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "terraformsastore"
     container_name       = "tfstate"
     key                  = "k8.tfstate"
-    access_key = "3Z39FcSFo607a94o5JD4e1S6/i0qj4/2SxtRaK9V4lCZThLoJWexwwAEdMG9i3cmP8YibD1o9K5Aqd7EwEBGqA=="
+    access_key = {}
   }
   required_providers {
     azurerm = {
